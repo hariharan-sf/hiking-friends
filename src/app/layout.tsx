12ea1fc3875d5f9d4ea2e2278ae1d9cf6cc50ee5
Hariharan <hariharan@serviceform.com>
2026-03-08 09:47:27 +0530

    initialize app

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Hiking Friends Sri Lanka",
+  description: "Hiking Friends Sri Lanka",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
